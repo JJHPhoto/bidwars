e801d3b2fed33362f6c48ff18559db75e9b18e9e
Joshua J. Haller <phone@joshuajhaller.com>
2020-12-19 09:12:56 -0800

    Initial files.

---
+++ biddingSeeds.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS bid_warsDB;
+
+CREATE DATABASE bid_warsDB;
+
+USE bid_warsDB;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  flavor VARCHAR(45) NULL,
+  price DECIMAL(10,2) NULL,
+  quantity INT NULL,
+  PRIMARY KEY (id)
+);
+
+/* just taken from icecream db. we'll update in project */
+
+INSERT INTO products (flavor, price, quantity)
+VALUES ("vanilla", 2.50, 100);
+
+INSERT INTO products (flavor, price, quantity)
+VALUES ("chocolate", 3.10, 120);
+
+INSERT INTO products (flavor, price, quantity)
+VALUES ("strawberry", 3.25, 75);
